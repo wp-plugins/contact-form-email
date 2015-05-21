cf27f52944d9a96ad659ab4973b2939875dc9b07
jeffbullins <jeffbullins@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-21 04:05:53 +0000

    Added apply_filters('cf_add_to_form_filter')
    git-svn-id: https://plugins.svn.wordpress.org/contact-form-email/tags/1.1@1164643 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/think_button_modal.php
@@ -0,0 +1,23 @@
+
+<html>
+<head>
+<title>
+testingt
+</title>
+
+</head>
+<body>
+<h3>Copy the button shortcode you would like to use and paste it into your Wordpress editor</h3>
+<p>[think_button url="http://www.google.com" text="Get Access Now Baby"][/think_button]
+
+<p>[think_button url="http://www.google.com" text="Get Access Now Baby" color="red" font_size="30px" ][/think_button]
+
+<p>[think_button url="http://www.google.com" text="Get Access Now Baby" color="yellow" font_size="30px" ][/think_button]
+
+<p>[think_button url="http://www.google.com" text="Get Access Now Baby" color="blue"][/think_button]
+
+<p>[think_button url="http://www.google.com" text="Get Access Now Baby" color="green"][/think_button]
+
+<p>[think_button url="http://www.google.com" text="Get Access Now Baby" color="purple"][/think_button]
+</body>
+</html>
